275bff75d393e32734fc23ea76d7a81de8254928
yarden.shacham <yarden101111@gmail.com>
2018-03-21 09:11:15 +0200

    merge fixes

--- src/components/calendar-month/calendar-month.component.jsx
+++ src/components/calendar-month/calendar-month.component.jsx
@@ -1,73 +1,71 @@
 import React from "react";
 
-const Day = (props) => {
-    const {isCurrentMonth, number, isLastStartWeek} = props.dayInfo;
+const Day = ({onChange, dayInfo}) => {
+    const {isCurrentMonth, number, date} = dayInfo;
     return (
-        <div className={`day ${isCurrentMonth ? 'currentMonth' : ''} `}>
-            {number}
+        <div onClick={() => onChange(date)}
+             className={`day ${isCurrentMonth ? 'currentMonth' : ''} `}>
+        {number}
         </div>
-    );
-};
+        );
+    };
 
-const Week = (props) => {
-    const {days} = props;
-    return (<div className="week">{days.map((d, i) => <Day key={i} dayInfo={d}/>)}</div>);
-};
-
-const DaysOfWeek = (props) => (
-    <div className="daysOfWeek">
-        {props.daysOfWeek.map((d, i) => <div key={i} className="day title">{d}</div>)}
-    </div>
+const Week = ({onChange, days}) => (
+    <div className="week">{days.map((d, i) => <Day key={i} onChange={onChange}
+                                                   dayInfo={d}/>)}</div>
 );
 
-
-const CalendarNavigator = (props) => {
-    const {currentMonth, nextMonth, previousMonth} = props;
-    return (
-        <div className="navigator-container">
-            <button className="move-month" onClick={(e) => {
-                e.preventDefault();
-                previousMonth();
-            }}>
-                <i className="arrow left"></i>
-            </button>
-            <div className="current-date">{currentMonth}</div>
-            <button className="move-month" onClick={(e) => {
-                e.preventDefault();
-                nextMonth();
-            }}>
-                <i className="arrow right"></i>
-            </button>
-        </div>
+const DaysOfWeek = ({daysOfWeek}) => (
+    <div className="daysOfWeek">
+        {daysOfWeek.map((d, i) => <div key={i} className="day title">{d}</div>)}
+    </div>
     );
-};
+
+
+    const CalendarNavigator = (props) => {
+        const {currentMonth, nextMonth, previousMonth} = props;
+        return (
+            <div className="navigator-container">
+                <button className="move-month" onClick={(e) => {
+                    e.preventDefault();
+                    previousMonth();
+                }}>
+                    <i className="arrow left"></i>
+                </button>
+                <div className="current-date">{currentMonth}</div>
+                <button className="move-month" onClick={(e) => {
+                    e.preventDefault();
+                    nextMonth();
+                }}>
+                    <i className="arrow right"></i>
+                </button>
+            </div>
+        );
+    }
+;
 
 const CalendarTitle = (props) => {
     const {daysOfWeek} = props;
     return (
-        <div className="calendar-title">
-            <CalendarNavigator {...props}/>
-        </div>
+    <div className="calendar-title">
+    <CalendarNavigator {...props}/>
+    </div>
     );
 };
-const CalendarContent = (props) => {
-    const {daysOfWeek, weeks} = props;
-    return (
-        <div className="weeks-container">
-            <DaysOfWeek daysOfWeek={daysOfWeek}/>
-            {weeks.map((week, i) => <Week key={i} days={week}/>)}
-        </div>
-    );
-}
+const CalendarContent = ({daysOfWeek, weeks, onChange}) =>
+    <div className="weeks-container">
+        <DaysOfWeek daysOfWeek={daysOfWeek}/>
+        {weeks.map((week, i) => <Week key={i} onChange={onChange} days={week}/>)}
+    </div>
 
 export class CalendarMonth extends React.Component {
 
     render() {
-        const {weeks, id} = this.props;
-        return (
-            <div ref="calendar" id={id} className="calendar-month">
-                <CalendarTitle{...this.props} />
-                <CalendarContent {...this.props}/>
-            </div>);
-    }
+    const {weeks, id} = this.props;
+    return (
+    <div ref="calendar" id={id} className="calendar-month">
+    <CalendarTitle{...this.props} />
+    <CalendarContent {...this.props}/>
+    </div>);
+}
 }
